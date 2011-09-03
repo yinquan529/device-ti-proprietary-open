27589d86d6ebbf122703eb8183333187320f698c
Vishal Bhoj <vishal.bhoj@linaro.org>
2011-09-03 11:33:18 +0400

    move wifi firmware path from lib/ to etc/.fixes firmware load timeout issue.
    Change-Id: I5f4e9bfc3823d47d0faf2aa835628ac36fd37afc

--- wifi/omap4/system/Android.mk
+++ wifi/omap4/system/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-copy_files_from := $(wildcard $(LOCAL_PATH)/lib/firmware/ti-connectivity/*)
+copy_files_from := $(wildcard $(LOCAL_PATH)/etc/firmware/ti-connectivity/*)
 copy_files_to := $(copy_files_from:$(LOCAL_PATH)/%=$(TARGET_OUT)/%)
 $(copy_files_to): $(TARGET_OUT)/% : $(LOCAL_PATH)/% | $(ACP)
 	$(transform-prebuilt-to-target)
