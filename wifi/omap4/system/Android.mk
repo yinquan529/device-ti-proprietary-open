7aabac869651a1ec6dacc89a450c3947ec3f1676
Vishal Bhoj <vishal.bhoj@linaro.org>
2011-10-27 12:41:06 +0530

    wifi and BT: adding wifi firmware and updating BT firmware path
    wl1271-fw-3.bin is the firmware version for wifi driver in
    3.2-rc0-pre kernel. BT firmware are now located in
    etc/firmware/ti-connectivity/ folder as the path is updated
    in the kernel. BT firmware from etc/firmware/ path are
    not removed since that is the path referred in staging
    build.
    
    Change-Id: Ife0b8a377fd08323ba058cc367b21c34271fd447
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- wifi/omap4/system/Android.mk
+++ wifi/omap4/system/Android.mk
@@ -6,3 +6,6 @@ copy_files_to := $(copy_files_from:$(LOCAL_PATH)/%=$(TARGET_OUT)/%)
 $(copy_files_to): $(TARGET_OUT)/% : $(LOCAL_PATH)/% | $(ACP)
 	$(transform-prebuilt-to-target)
 ALL_PREBUILT += $(copy_files_to)
+
+PRODUCT_COPY_FILES += \
+        device/ti/proprietary-open/wifi/omap4/LICENCE.ti-connectivity:root/LICENCE.ti-connectivity
